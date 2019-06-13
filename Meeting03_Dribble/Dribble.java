2898ef9bac6ac778e5c1619125ad94aacf98b92d
louisalvaro70 <42855235+louisalvaro70@users.noreply.github.com>
2019-06-13 09:31:02 +0700

    Add files via upload

--- Meeting03_Dribble/Dribble.java
+++ Meeting03_Dribble/Dribble.java
@@ -1,6 +1,6 @@
-//package Meeting03_Dribble;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.util.ArrayList;
@@ -34,18 +34,62 @@ public class Dribble {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
+        JFrame commandLayout = new JFrame ("Add or Kill balls");
+        JLabel posX = new JLabel ("Position X");
+        posX.setBounds(10, 30, 130, 30);
+        JLabel posY = new JLabel ("Position Y");
+        posY.setBounds(10, 60, 130, 100);
+        JLabel setRadius = new JLabel ("Set Radius");
+        setRadius.setBounds(10, 90, 130, 30);
+        JLabel veloX = new JLabel ("Velocity X");
+        veloX.setBounds(10, 40, 130, 30);
+        JLabel veloY = new JLabel ("Velocity Y");
+        veloY.setBounds(10, 120, 130, 30);
+        JLabel addColor = new JLabel ("Color");
+        addColor.setBounds(10, 150, 130, 30);
+
+        commandLayout.add(posX);
+        commandLayout.add(posY);
+        commandLayout.add(setRadius);
+        commandLayout.add(veloX);
+        commandLayout.add(veloY);
+        commandLayout.add(addColor);
+
+        JButton commandButton = new JButton ("Create New Ball");
+        commandButton.setBounds(300, 300, 100, 20);
+
+        b.addActionListener(new ActionListener() {
+            public void actionPerformed (ActionEvent event) {
+                Color color2 = Color.BLACK;
+                double pX = Double.parseDouble(posX.getText());
+                double pY = Double.parseDouble(posY.getText());
+                double r = Double.parseDouble(radius.getText());
+                double vX = Double.parseDouble(velocityX.getText());
+                double vY = Double.parseDouble(velocityY.getText());
+                String col = colorSet.getText();
+                switch (col.toLowerCase()) {
+                    case "black":
+                        color2 = Color.BLACK;
+                        break;
+                    case "blue":
+                        color2 = Color.BLUE;
+                        break;
+                    case "red":
+                        color2 = Color.RED;
+                        break;
+                }
+            }
+        }
+
         // create the walls
         createWalls();
 
         // create the ball
         balls.add(new Ball(300, 200, 50, 10, 10, Color.blue));
-        balls.add(new Ball(300, 100, 20, 15, -3, Color.green));
-        balls.add(new Ball(300, 520, 13, 25, 14, Color.red));
-        balls.add(new Ball(300, 350, 50, 7, -5, Color.yellow));
-        balls.add(new Ball(300, 205, 35, 8, 6, Color.cyan));
-        balls.add(new Ball(300, 150, 64, 15, -9, Color.magenta));
-        balls.add(new Ball(300, 275, 45, 25, 24, Color.black));
-        balls.add(new Ball(300, 562, 50, 14, -12, Color.orange));
+        balls.add(new Ball(300, 100, 20, 3, -3, Color.green));
+        balls.add(new Ball(300, 500, 35, 15, 18, Color.yellow));
+        balls.add(new Ball(300, 355, 24, 6, 10, Color.red));
+		balls.add(new Ball(300, 455, 20, 3, 15, Color.cyan));
 
         drawingArea = new DrawingArea(frame.getWidth(), frame.getHeight(), balls, walls);
         frame.add(drawingArea);
@@ -60,7 +104,9 @@ public class Dribble {
         walls.add(new Wall(50, 600, 1300, 600, Color.black));  // horizontal bottom
         walls.add(new Wall(1300, 100, 1300, 600, Color.black));  // vertical right
         walls.add(new Wall(50, 600, 50, 100, Color.black));  // vertical left
+        walls.add(new Wall(1300, 100, 95, 595, Color.BLACK));
     }
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(Dribble::new);
